1a82f197a6bff222c5d5ca16f63eeaac7f40db24
Jesus Ruiz Gutierrez <jesse432007@gmail.com>
2026-08-17 14:13:31 -0700

    Add working menu loop

---
+++ project.py
@@ -0,0 +1,24 @@
+
+
+print("This is Study Buddy...")
+
+
+while True:
+    
+    print("a. create your own flash card")
+    print("b. test me?: ")
+    print("c. quit")
+    
+    option = input("Enter: ").lower()
+
+    if option == "a":
+        print("coming soon!")
+    elif option == "b":
+        print("coming soon!")
+    elif option == "c":
+        print("goodbye!")
+        break
+    else:
+        print("Invalid choice")
+        
+        
