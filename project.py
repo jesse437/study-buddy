a9ffbbcc3196059db769c9bc0ecc443f4457dcad
Jesus Ruiz Gutierrez <jesse432007@gmail.com>
2026-08-18 13:17:19 -0700

    Add flashcard creation with CSV persistence

--- project.py
+++ project.py
@@ -1,18 +1,31 @@
+import csv
+import os
 
 
 print("This is Study Buddy...")
 
-
 while True:
     
     print("a. create your own flash card")
-    print("b. test me?: ")
+    print("b. test me ")
     print("c. quit")
     
-    option = input("Enter: ").lower()
+    option = input("Select: ").lower()
 
     if option == "a":
-        print("coming soon!")
+        question = input("Enter a question: ")
+        answer = input("Enter an answer: ")
+        
+        file_exists = os.path.exists("flashcards.csv")
+        
+        
+        with open("flashcards.csv", "a") as file:
+            writer = csv.DictWriter(file, fieldnames=["question", "answer"])
+            if not file_exists:
+                writer.writeheader()
+            writer.writerow({"question": question, "answer": answer})
+            
+        
     elif option == "b":
         print("coming soon!")
     elif option == "c":
