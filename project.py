3d0f1e075638764eed3170d0eeb9becd31de8619
Jesus Ruiz Gutierrez <jesse432007@gmail.com>
2026-08-21 20:14:12 -0700

    Wrap menu loop in main()

--- project.py
+++ project.py
@@ -13,46 +13,48 @@ def create_card(question, answer):
         writer.writerow({"question": question, "answer": answer})
     return True
 
-print("This is Study Buddy...")
-
-while True:
+def main():
+    print("This is Study Buddy...")
     
-    print("a. create your own flash card")
-    print("b. test me ")
-    print("c. quit")
-    
-    option = input("Select: ").lower()
 
-    if option == "a":
-        question = input("Enter a question: ")
-        answer = input("Enter an answer: ")
-        create_card(question, answer)
+    while True:
+    
+        print("a. create your own flash card")
+        print("b. test me ")
+        print("c. quit")
+    
+        option = input("Select: ").lower()
+
+        if option == "a":
+            question = input("Enter a question: ")
+            answer = input("Enter an answer: ")
+            create_card(question, answer)
             
         
-    elif option == "b":
-        if not os.path.exists("flashcards.csv"):
-            print("No flashcards yet! Add some first.")
-        else:
-            score = 0
-            total = 0
-            with open("flashcards.csv") as file:
-                reader = csv.DictReader(file)
-                for row in reader:
-                    print(f"question: {row['question']}")
-                    answer = input("Enter answer!: ")
-                    if check_answer(answer, row['answer']):
-                        print("Correct!")
-                        score += 1
-                    else:
-                        print("Wrong!")
-                    total += 1
-                print(f"You got {score} out of {total}!")
+        elif option == "b":
+            if not os.path.exists("flashcards.csv"):
+                print("No flashcards yet! Add some first.")
+            else:
+                score = 0
+                total = 0
+                with open("flashcards.csv") as file:
+                    reader = csv.DictReader(file)
+                    for row in reader:
+                        print(f"question: {row['question']}")
+                        answer = input("Enter answer!: ")
+                        if check_answer(answer, row['answer']):
+                            print("Correct!")
+                            score += 1
+                        else:
+                            print("Wrong!")
+                        total += 1
+                    print(f"You got {score} out of {total}!")
         
-    elif option == "c":
-        print("goodbye!")
-        break
-    else:
-        print("Invalid choice")
+        elif option == "c":
+            print("goodbye!")
+            break
+        else:
+            print("Invalid choice")
         
         
 if __name__ == "__main__":
